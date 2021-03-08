122d0b032a5af36090fa52bd82e5b982b6321f7b
ksdevinney <katie@devinney.org>
2021-03-07 20:58:33 -0600

    start columns, text boxes, and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 //use moment to display current date at the top
+let timeStamp = $("#currentDay");
+timeStamp.text(moment().format("dddd MMMM Do YYYY"));
+
+//hourly timeblocks
+
+//timeblocks are color coded
+
+//enter events in each hour on click
+
+//events saved to local storage

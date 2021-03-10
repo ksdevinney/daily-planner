5b014d0666981645b46d0d91588c79e76d18274c
ksdevinney <katie@devinney.org>
2021-03-09 21:35:03 -0600

    everything looks nicer and local storage works

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,21 @@ timeStamp.text(moment().format("dddd MMMM Do YYYY"));
 //hourly timeblocks
 
 //timeblocks are color coded
+let timeColor = $(".timeblock");
 
-//enter events in each hour on click
+//text in textboxes
+let textEl = $(".planner");
+let saveButton = $(".btn");
+
 
 //events saved to local storage
+function saveEvent(event) {
+    event.preventDefault();
+    var eventInput = $(event.target).parent().children(".planner");
+    //want this to save text in the box to local storage
+    console.log(eventInput.val());
+    var timeSlot = $(event.target).parent().attr("id");
+    console.log(timeSlot);
+    localStorage.setItem(timeSlot, eventInput.val());
+}
+saveButton.on("click", saveEvent)

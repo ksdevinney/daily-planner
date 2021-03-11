2d8b2bdcc08721df55ec7034281377e897af21e2
ksdevinney <katie@devinney.org>
2021-03-10 21:38:08 -0600

    local storage displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 //use moment to display current date at the top
 let timeStamp = $("#currentDay");
-timeStamp.text(moment().format("dddd MMMM Do YYYY"));
+timeStamp.text(moment().format("dddd, MMMM Do, YYYY"));
 
 //hourly timeblocks
 
 //timeblocks are color coded
-let timeColor = $(".timeblock");
+//let timeColor = $(".timeblock");
+var getTime = (moment().format("H"));
 
 //text in textboxes
 let textEl = $(".planner");
@@ -16,10 +17,22 @@ let saveButton = $(".btn");
 function saveEvent(event) {
     event.preventDefault();
     var eventInput = $(event.target).parent().children(".planner");
-    //want this to save text in the box to local storage
+    //save text in the box to local storage
     console.log(eventInput.val());
     var timeSlot = $(event.target).parent().attr("id");
     console.log(timeSlot);
     localStorage.setItem(timeSlot, eventInput.val());
-}
+    }
+
+function retrieveEvents () {
+    for(var i=8; i<=17; i++ ) {
+        $("#" + i).children(".planner").text(localStorage.getItem(i));
+    }}
+
+retrieveEvents();
+
+
 saveButton.on("click", saveEvent)
+
+//events displayed from local storage
+//for loop?

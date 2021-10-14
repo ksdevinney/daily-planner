5e2e284de15fc43ad30e9e356d60eee649aa794c
ksdevinney <katie@devinney.org>
2021-10-13 19:57:17 -0500

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ timeStamp.text(moment().format("dddd, MMMM Do, YYYY"));
 
 //timeblocks are color coded
 let timeColor = $(".timeblock");
-var getTime = (moment().format("H"));
+let getTime = (moment().format("H"));
 
 console.log(getTime);
 
@@ -16,11 +16,9 @@ function colorCode () {
         let colorBlock = $("#" + id).children(".planner").text(localStorage.getItem(id));
         if (id == getTime) {
             colorBlock.addClass("present")
-        }
-        else if (id < getTime) {
+        } else if (id < getTime) {
             colorBlock.addClass("past")
-        }
-        else if (id > getTime) {
+        } else if (id > getTime) {
             colorBlock.addClass("future")
         }
     }
@@ -31,20 +29,12 @@ colorCode();
 //events saved to local storage
 function saveEvent(event) {
     event.preventDefault();
-    var eventInput = $(event.target).parent().children(".planner");
+    let eventInput = $(event.target).parent().children(".planner");
     //save text in the box to local storage
     console.log(eventInput.val());
-    var timeSlot = $(event.target).parent().attr("id");
+    let timeSlot = $(event.target).parent().attr("id");
     console.log(timeSlot);
     localStorage.setItem(timeSlot, eventInput.val());
 }
-
-// function retrieveEvents () {
-//     for(var i = 8; i <= 17; i++) {
-//         $("#" + i).children(".planner").text(localStorage.getItem(i))//.addClass(className);
-//     }
-// }
-
-// retrieveEvents();
 
 saveButton.on("click", saveEvent)

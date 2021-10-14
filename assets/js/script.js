13db7c681690978866f3803c3383ee7b864ac8bf
ksdevinney <katie@devinney.org>
2021-10-13 20:09:54 -0500

    variable types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //save button
-let saveButton = $(".btn");
+const saveButton = $(".btn");
 
 //use moment to display current date at the top
-let timeStamp = $("#currentDay");
+const timeStamp = $("#currentDay");
 timeStamp.text(moment().format("dddd, MMMM Do, YYYY"));
 
 //timeblocks are color coded
-let timeColor = $(".timeblock");
-let getTime = (moment().format("H"));
+const timeColor = $(".timeblock");
+const getTime = (moment().format("H"));
 
 console.log(getTime);
 
@@ -29,10 +29,10 @@ colorCode();
 //events saved to local storage
 function saveEvent(event) {
     event.preventDefault();
-    let eventInput = $(event.target).parent().children(".planner");
+    const eventInput = $(event.target).parent().children(".planner");
     //save text in the box to local storage
     console.log(eventInput.val());
-    let timeSlot = $(event.target).parent().attr("id");
+    const timeSlot = $(event.target).parent().attr("id");
     console.log(timeSlot);
     localStorage.setItem(timeSlot, eventInput.val());
 }
